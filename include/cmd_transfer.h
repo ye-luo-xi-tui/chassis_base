2bffdbd1b28f9dc4a61b51bb54cd26cf2ae70830
yezi <1536117624@qq.com>
2021-07-27 20:27:35 +0800

    增加了按下按键后急停的功能。

--- include/cmd_transfer.h
+++ include/cmd_transfer.h
@@ -8,8 +8,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
-#include <string>
-using namespace std;
+
 namespace chassis_base {
 
 class cmd_transfer {
@@ -17,19 +16,14 @@ class cmd_transfer {
   explicit cmd_transfer(ros::NodeHandle root_nh);
  private:
   bool IS_STOP_{false};
-  bool IS_STICK_{false};
   geometry_msgs::Twist twist_{};
-  int stop_button_{};
-  int start_button_{};
-  int auto_button_{};
-  int stick_button_{};
+  int stop_button{};
+  int start_button{};
   ros::Publisher cmd_pub_{};
   ros::Subscriber joy_sub_{};
-  ros::Subscriber joy_cmd_sub_{};
-  ros::Subscriber computer_cmd_sub_{};
+  ros::Subscriber cmd_sub_{};
   void joy_callback(const sensor_msgs::Joy::ConstPtr &joy);
-  void joy_cmd_callback(const geometry_msgs::Twist::ConstPtr &cmd);
-  void computer_cmd_callback(const geometry_msgs::Twist::ConstPtr &cmd);
+  void cmd_callback(const geometry_msgs::Twist::ConstPtr &cmd);
   void update();
 };
 }
