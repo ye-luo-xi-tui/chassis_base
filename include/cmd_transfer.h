2f68e04b0d4f665f9e9ad5c205ef7b7ae7c6bc48
yezi <1536117624@qq.com>
2021-09-09 15:07:40 +0800

    更改吴老师底盘相关配置

--- include/cmd_transfer.h
+++ include/cmd_transfer.h
@@ -8,7 +8,8 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
-
+#include <string>
+using namespace std;
 namespace chassis_base {
 
 class cmd_transfer {
@@ -16,14 +17,19 @@ class cmd_transfer {
   explicit cmd_transfer(ros::NodeHandle root_nh);
  private:
   bool IS_STOP_{false};
+  bool IS_STICK_{false};
   geometry_msgs::Twist twist_{};
-  int stop_button{};
-  int start_button{};
+  int stop_button_{};
+  int start_button_{};
+  int auto_button_{};
+  int stick_button_{};
   ros::Publisher cmd_pub_{};
   ros::Subscriber joy_sub_{};
-  ros::Subscriber cmd_sub_{};
+  ros::Subscriber joy_cmd_sub_{};
+  ros::Subscriber computer_cmd_sub_{};
   void joy_callback(const sensor_msgs::Joy::ConstPtr &joy);
-  void cmd_callback(const geometry_msgs::Twist::ConstPtr &cmd);
+  void joy_cmd_callback(const geometry_msgs::Twist::ConstPtr &cmd);
+  void computer_cmd_callback(const geometry_msgs::Twist::ConstPtr &cmd);
   void update();
 };
 }
