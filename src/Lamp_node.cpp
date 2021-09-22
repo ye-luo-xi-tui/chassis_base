c4249807b4485154046f5c4688f0d28ea2aa8baa
yezi <1536117624@qq.com>
2021-09-22 14:41:46 +0800

    完善车灯程序，修改一些配置文件使之更适应第二版底盘

--- src/Lamp_node.cpp
+++ src/Lamp_node.cpp
@@ -6,16 +6,36 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 
-// void joy_callback(const sensor_msgs::Joy::ConstPtr &joy) { if
-// (joy->buttons[]) }
+GpioMangager gpio_manager;
+bool Front_lamp_is_light = false;
+bool Back_lamp_is_light = false;
+
+void joy_callback(const sensor_msgs::Joy::ConstPtr &joy) {
+  if (joy->buttons[4]) {
+    if (Front_lamp_is_light) {
+      gpio_manager.writeOutput(248, false);
+      Front_lamp_is_light = false;
+    } else {
+      gpio_manager.writeOutput(248, true);
+      Front_lamp_is_light = true;
+    }
+  }
+  if (joy->buttons[5]) {
+    if (Back_lamp_is_light) {
+      gpio_manager.writeOutput(249, false);
+      Back_lamp_is_light = false;
+    } else {
+      gpio_manager.writeOutput(249, true);
+      Back_lamp_is_light = true;
+    }
+  }
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "Lamp_node");
   ros::NodeHandle nh("/lamp_node");
-  GpioMangager gpio_mangager;
-  gpio_mangager.init(nh);
-  //  ros::Subscriber joy_sub =
-  //      nh.subscribe<sensor_msgs::Joy>("joy", 10, &joy_callback);
-
+  ros::NodeHandle root_nh;
+  gpio_manager.init(nh);
+  ros::Subscriber joy_sub = root_nh.subscribe("joy", 10, joy_callback);
   ros::spin();
 }
